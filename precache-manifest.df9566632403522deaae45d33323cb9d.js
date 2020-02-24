58596173b7e32cb5aaf41436e26b264bcb8acf48
laksuryann <venkatadharma1@gmail.com>
2020-02-24 09:38:22 +0530

    Updates

---
+++ precache-manifest.df9566632403522deaae45d33323cb9d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19890fdf331caaaf67cf7492e8b51db6",
+    "url": "/online-auction-hub/index.html"
+  },
+  {
+    "revision": "d70db30dd9321a7aaa12",
+    "url": "/online-auction-hub/static/css/main.c913235a.chunk.css"
+  },
+  {
+    "revision": "025b633a68df7302185a",
+    "url": "/online-auction-hub/static/js/2.043ca380.chunk.js"
+  },
+  {
+    "revision": "d9c45e99246f9278c0aaaed8f70e87b4",
+    "url": "/online-auction-hub/static/js/2.043ca380.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d70db30dd9321a7aaa12",
+    "url": "/online-auction-hub/static/js/main.0ea46f94.chunk.js"
+  },
+  {
+    "revision": "b4f984e1982ac99cd08d",
+    "url": "/online-auction-hub/static/js/runtime-main.8b32b3c4.js"
+  },
+  {
+    "revision": "646e3024ab127f340fe026061bc6c571",
+    "url": "/online-auction-hub/static/media/back.646e3024.svg"
+  }
+]);
